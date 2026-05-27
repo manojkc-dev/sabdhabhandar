caca87ecb13e7952e8a6a3766a14a1ddb78c4835
Your Name <your@email.com>
2026-05-27 20:24:33 +0545

    Fix: Add ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- sabdhabhandar/settings.py
+++ sabdhabhandar/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://sabdhabhandar.onrender.com']
